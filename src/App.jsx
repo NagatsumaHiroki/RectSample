daab84cd92647a72125154a2c499acffe95f63d2
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-07-11 15:31:34 +0900

    2021_07_11:学習 ・カウントアップ機能 ・表示機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Colorfull from "./components/Colorfull";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(true);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwichShowFlag = () => {
+    setfaceShowFlag(!faceShowFlag);
+  };
   const contetLedyStyle = {
     color: "pink",
     fontSize: "18px"
   };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setfaceShowFlag(true);
+    } else {
+      faceShowFlag && setfaceShowFlag(false);
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <Colorfull color="blue" message="お元気ですか" />
       <p style={contetLedyStyle}>元気です</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickSwichShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>!(^^)!</p>}
     </React.Fragment>
   );
 };
